d1aea1a7ea2fb49e0f28f3319b26e35bd875b36f
Nitin Kaveriappa Udiyanda Muthanna <nitinkaveriappa@gmail.com>
2017-02-17 22:22:39 -0500

    Update

--- data_store.php
+++ data_store.php
@@ -23,17 +23,17 @@ class store_incoming
 		if($sum == 0){
 			$desktop=true;
 		}
-		
+
 		//if A or B
 		//if($verifyCount > 0 || $desktop=true) {
-		
+
 			//Do nothing
 		//}
 		//else {
 			//Add the new data into database
 			//accelerometer_data
 			$addAccelData = $connection->prepare("INSERT INTO accelerometer_data(ip_address, acc_x, acc_y, acc_z, acc_i, acc_alpha, acc_beta, acc_gamma) VALUES(:ip,:x,:y,:z,:i,:a,:b,:g);");
-			$addAccelData->bindParam(':ip',$playerId);
+			$addAccelData->bindParam(':ip',$remoteip);
 			$addAccelData->bindParam(':x',$values[0]);
 			$addAccelData->bindParam(':y',$values[1]);
 			$addAccelData->bindParam(':z',$values[2]);
@@ -44,7 +44,7 @@ class store_incoming
 			$addAccelData->execute();
 
 			$addGyroData = $connection->prepare("INSERT INTO gryoscope_data(ip_address, gyro_alpha, gyro_beta, gyro_gamma) VALUES(:ip,:alpha,:beta,:gamma);");
-			$addGyroData->bindParam(':ip',$playerId);
+			$addGyroData->bindParam(':ip',$remoteip);
 			$addGyroData->bindParam(':alpha',$values[7]);
 			$addGyroData->bindParam(':beta',$values[8]);
 			$addGyroData->bindParam(':gamma',$values[9]);
@@ -67,11 +67,11 @@ class store_incoming
 		$verifyCount = $verifyQuery->rowCount();
 
 		//if A
-		if($verifyCount > 0) 
+		if($verifyCount > 0)
 		{
 			//Do nothing
 		}
-		else 
+		else
 		{
 			//Add the new user_data into database
 			$addUserData = $connection->prepare("INSERT INTO user_data(ip_address, name, email) VALUES(:ip,:name,:email);");
--- submit_data.php
+++ submit_data.php
@@ -21,10 +21,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 		header("Location:index.html?type=err");
 	}
 
-	$remoteip= $_SERVER['REMOTE_ADDR'];
+	//Verifies the captcha is set
+	if(isset($_POST['g-recaptcha-response']))
+	{
+		$captcha=$_POST['g-recaptcha-response'];
+	}
+	else
+	{
+		header("Location:index.html?type=err");
+	}
 
-	include('data_store.php');
-	$submit_data = new store_incoming();
-  $submit_data->user_data($name,$email,$remoteip);
+	//get captcha secret
+	$config = parse_ini_file('config.php');
+	$secret = $config['secret'];
+	//Validates the captcha value from google server
+	$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+	$data = json_decode($response);
+
+	//If its a bot it redirects to index
+	if($data->success==false)
+     {
+       header("Location:contactme.html?type=err");
+     }
+	//If not a bot 	then tries to login in player
+	else {
+		$remoteip= $_SERVER['REMOTE_ADDR'];
+
+		include('data_store.php');
+		$submit_data = new store_incoming();
+	  $submit_data->user_data($name,$email,$remoteip);
+	}
 }
 ?>
